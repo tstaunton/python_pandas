64d3d046851c4e3b8d8dc9e5d667b37cc0e13f6b
Tony Staunton <tony.staunton@gmail.com>
2018-10-26 16:24:47 +0100

    Lecture on how to create a DataFrame from a dictionary

--- 02 Introduction to Pandas/DataFrames/s2_6_dataframes_from_dictionary.py
+++ 02 Introduction to Pandas/DataFrames/s2_6_dataframes_from_dictionary.py
@@ -1,32 +1,38 @@
 import pandas as pd
 
+# Create a dictionary of sales data
 # course_sales = {'course':['Python','Ruby','Excel','C++'],
 #                'day':['Mon','Tue','Wed','Tue'],
 #                'price':[5,10,15,20],
 #                'sale':[2,3,5,7]
 #                }
 
+# Convert sales data into a DataFrame
 # df_sales = pd.DataFrame(course_sales)
 # print(df_sales)
 
-course = ['Python','Ruby','Excel','C++']
+# Create indidivdual lists from sales data
+# course = ['Python','Ruby','Excel','C++']
 
-day = ['Mon', 'Tue', 'Tue', 'Wed']
+# day = ['Mon', 'Tue', 'Tue', 'Wed']
 
-price = [5,10,15,20]
+# price = [5,10,15,20]
 
-sale = [2,3,5,7]
+# sale = [2,3,5,7]
 
-column_labels = ['Course', 'Day', 'Price', 'Sale']
+# Create a list of column labels
+# column_labels = ['Course', 'Day', 'Price', 'Sale']
 
-column_names = [course,day,price,sale]
+# Add a list of column entries for each column
+# column_names = [course,day,price,sale]
 
-master_list = list(zip(column_labels,column_names))
+# Combine list of column labels and column names above to create a new single list
+# master_list = list(zip(column_labels,column_names))
+# print(master_list)
 
-print(master_list)
+# Convert master list to a dictionary
+# sales_data = dict(master_list)
 
-sales_data = dict(master_list)
-
-df_sales = pd.DataFrame(sales_data)
-
-print(df_sales)
+# Convert master list dictionary to a DataFrame
+# df_sales = pd.DataFrame(sales_data)
+# print(df_sales)
