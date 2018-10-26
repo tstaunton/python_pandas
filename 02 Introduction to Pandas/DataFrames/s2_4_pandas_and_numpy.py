3a1aad046762f80336c3ef06bce047d5893c9616
Tony Staunton <tony.staunton@gmail.com>
2018-10-26 13:39:15 +0100

    Lecture on using Numpy and Pandas together

--- 02 Introduction to Pandas/DataFrames/s2_4_pandas_and_numpy.py
+++ 02 Introduction to Pandas/DataFrames/s2_4_pandas_and_numpy.py
@@ -2,13 +2,12 @@ import pandas as pd
 
 df = pd.read_csv('../../sample_data/02 Introduction to Pandas/intel.csv')
 
-# Create a series
-# open = df['Open']
-# print(type(open))
+# Print the Open column
+# open =df['Open']
 
-# Confirm NumPy Array
-# print(open.head())
+# Create a new variable and print its type to confirm it's a numpy array
 # new_open = open.values
 # print(type(new_open))
 
-# print(new_open)
+# Print the values contained in new_open
+print(new_open)
