fea4c04baa706eb6bcc9c6792f2da237a101b12a
Tony Staunton <tony.staunton@gmail.com>
2018-10-26 16:07:31 +0100

    Lecture on creating a DataFrame from a numpy array

--- 02 Introduction to Pandas/DataFrames/s2_5_dataframes_from_numpy.py
+++ 02 Introduction to Pandas/DataFrames/s2_5_dataframes_from_numpy.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
 
-new_array = np.arange(0,10).reshape(2,5)
+# Create a numpy array
+# new_array = np.arange(0,10).reshape(2,5)
 # print(new_array)
 
-# Convert to a DataFrame
-# df = pd.DataFrame(data=new_array)
+# Convert numpy array into DataFrame
+# df = pd.DataFrame(data=new_array, columns=['A', 'B', 'C', 'D', 'E'])
 # print(df)
-
-# Add column names
-df = pd.DataFrame(data=new_array, columns=['A','B','C','D','E'])
-print(df)
