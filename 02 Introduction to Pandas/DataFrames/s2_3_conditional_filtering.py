8ca5ec69e44e39e3eaac41764a2d7a3c719a2556
Tony Staunton <tony.staunton@gmail.com>
2018-10-26 12:42:57 +0100

    Lecture on using condtional filtering

--- 02 Introduction to Pandas/DataFrames/s2_3_conditional_filtering.py
+++ 02 Introduction to Pandas/DataFrames/s2_3_conditional_filtering.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 df = pd.read_csv('../../sample_data/02 Introduction to Pandas/intel.csv')
 
-# Conditional Filtering
-my_open = df['Open'] > 48.0
-print(my_open)
+# Create a conditional to return data over 40.0
+# my_open = df['Open'] > 40
+# print(my_open)
 
-# Show DataFrame where only my_open is True
+# Print non-boolean values of variable my_open
 # print(df[my_open])
 
-# One step option
-# print(df[df['Open'] > 55.0])
+# Perform the above with one line of code
+# print(df[df['Open'] < 55.0])
