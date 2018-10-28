773b2deb858a29630943075d1e14ab2de13a9e0a
Tony Staunton <tony.staunton@gmail.com>
2018-10-28 21:03:13 +0000

    Lecture on Scatter Plots

--- 03 Data Analysis/s3_3_scatter_plots.py
+++ 03 Data Analysis/s3_3_scatter_plots.py
@@ -4,15 +4,20 @@ import matplotlib.pyplot as plt
 # Import our data file
 life = pd.read_csv('../sample_data/03 Data Analysis/irish_life_expec.csv')
 
+# Print DataFrame
+# print(life)
+
+# Create Scatter Plot with x axis = year and y axis = life expectancy
 life.plot(kind='scatter', x='year', y='life expec')
 
 # Add the title
-plt.title('Irish Life Expectancy 2000 - 2014')
+plt.title('Irish Life Expectancy')
 
 # Add the x-axis label
 plt.xlabel('Age')
 
 # Add the y-axis label
-plt.ylabel('Year')
+plt.ylabel('Life Expectancy')
 
+# Show Plot
 plt.show()
