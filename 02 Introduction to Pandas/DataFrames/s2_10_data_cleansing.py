652e43dbc13ba522630d6a32c963bbf7d68eeaed
Tony Staunton <tony.staunton@gmail.com>
2018-10-28 18:53:27 +0000

    Updated lectures on data cleansing

--- 02 Introduction to Pandas/DataFrames/s2_10_data_cleansing.py
+++ 02 Introduction to Pandas/DataFrames/s2_10_data_cleansing.py
@@ -2,30 +2,20 @@ import pandas as pd
 
 # df = pd.read_csv('../../sample_data/02 Introduction to Pandas/edited_googleplaystore_dataset.csv')
 
-# Create a string filepath
+column_names = ['App', 'Rating', 'Reviews', 'Size', 'Number of Install', 'Type', 'Price', 'Last Updated']
+
 filepath = ('../../sample_data/02 Introduction to Pandas/edited_googleplaystore_dataset.csv')
 
-column_names = ['App','Rating','Reviews', 'Size','# of Installs','Type','Price','Last Updated']
-
-# Create DataFrame
 df = pd.read_csv(filepath, header=None, names=column_names, na_values='-1')
 
-df.index = df['App']
+df.index = df['Last Updated']
 
-new_columns = ['Rating','Reviews', 'Size','# of Installs','Type','Price','Last Updated']
+new_columns = ['App', 'Reviews']
 
 df = df[new_columns]
 
-# print(df.head())
+out_csv = 'Google Play Data'
+df.to_csv(out_csv)
 
-# out_csv = 'Google Play Data'
-# df.to_csv(out_csv)
-
-out_xlsx = 'Google Play Data.xlsx'
+out_xlsx = 'Google Play Data EXCEL.xlsx'
 df.to_excel(out_xlsx)
-
-# Info about our CSV file
-# print(df.info())
-
-# View the first 30 rows with .head()
-# print(df.head(30))
