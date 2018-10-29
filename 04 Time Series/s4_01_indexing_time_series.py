47cdcb41dc38ac3f6ceaafaf2749564d744fe813
Tony Staunton <tony.staunton@gmail.com>
2018-10-29 12:49:23 +0000

    Lecture on Indexing Pandas Time Series

--- 04 Time Series/s4_01_indexing_time_series.py
+++ 04 Time Series/s4_01_indexing_time_series.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 sales_data = pd.read_csv('../sample_data/04 Time Series/sales_data.csv', parse_dates=True, index_col='InvoiceDate')
 
-# print(sales_data)
+
+# Print sales_data DataFrame to check it's working
+# print(sales_data.head())
 
 
-# morning_sale = sales_data.loc['2010-12-01 08:35:00', 'Description']
+# Print .info()
+# print(sales_data.info())
 
-# morning_sale = sales_data.loc['2010-12-01 08:26:00']
 
-morning_sales = sales_data.loc['2010-12-01 08:26:00':'2010-12-01 09:00:00']
-
-print(morning_sales)
+# Use .loc accessor
+# morning_sale = sales_data.loc['2010-12-01 08:26:00':'2010-12-01 09:00:00']
+# print(morning_sale)
