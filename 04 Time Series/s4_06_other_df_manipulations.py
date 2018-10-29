07908b70dee25e20258cfb33a878bbc48e2de9d2
Tony Staunton <tony.staunton@gmail.com>
2018-10-29 19:04:09 +0000

    Leacture on Further Filtering Options

--- 04 Time Series/s4_06_other_df_manipulations.py
+++ 04 Time Series/s4_06_other_df_manipulations.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 sales_data = pd.read_csv('../sample_data/04 Time Series/sales_data.csv', parse_dates=True, index_col='InvoiceDate')
 
+# print(sales_data.head())
 search = sales_data['Description'].str.contains('POPPY')
 
 total_poppy_sales = search.resample('H').sum()
