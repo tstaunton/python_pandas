5a8e6ee1954428dd252d138e902095966eecf7b2
Tony Staunton <tony.staunton@gmail.com>
2018-10-29 21:36:46 +0000

    Lecture on Plotting Time with Pandas

--- 04 Time Series/s4_07_plotting_time_data.py
+++ 04 Time Series/s4_07_plotting_time_data.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 
 stock_price = pd.read_csv('../sample_data/04 Time Series/intel.csv', parse_dates=True, index_col='Date')
 
-stock_price.loc['2017-10',['Open','Close']].plot(subplots=True, title='Intel')
+stock_price.loc['2017-10-16':'2017-10-20',['Open','Close']].plot(style='.-', title='Intel', subplots=True)
 plt.ylabel('Closing Price')
 plt.show()
