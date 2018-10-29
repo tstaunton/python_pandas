4f607a9174bae85fdc84bba29f5663098bb9817f
Tony Staunton <tony.staunton@gmail.com>
2018-10-29 15:25:11 +0000

    Updated Resampling Lecture

--- 04 Time Series/s4_04_resampling.py
+++ 04 Time Series/s4_04_resampling.py
@@ -2,10 +2,7 @@ import pandas as pd
 
 sales_data = pd.read_csv('../sample_data/04 Time Series/resampling_sales_data.csv', parse_dates=True, index_col='InvoiceDate')
 
-weekly_mean = sales_data.loc[:,'Quantity'].resample('2W').sum()
+weekly_mean = sales_data.resample('M').sum()
+
 
 print(weekly_mean)
-
-# print(weekly_mean.loc['2010-10-31'])
-
-# print(weekly_mean.loc['2010-01-24', 'Quantity'])
